8649c80e997d845e12d9ed1c22476ffd9b7b287f
Joao Victor <victorcostamartins0@gmail.com>
2026-06-22 23:26:16 -0300

    first commit

---
+++ app/Models/Pedidos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pedidos extends Model
+{
+    protected $fillable = [
+        'cliente_nome',
+        'email',
+        'produto',
+        'quantidade',
+        'key_idempotencia'
+    ];
+}
